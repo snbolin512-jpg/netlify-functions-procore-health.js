09f29534fc80462be4e596c2847f58684dacaa1a
snbolin512-jpg <snbolin512@gmail.com>
2026-08-20 11:17:59 -0400

    delete and rerun ohmboy

--- ohmboy-clear.js
+++
@@ -1,5 +0,0 @@
-const { json, clearPrefix, EVENT_PREFIX } = require("./_lib/store");
-exports.handler = async function () {
-  const cleared = await clearPrefix(EVENT_PREFIX);
-  return json(200, { ok: true, cleared });
-};
--- ohmboy-events.js
+++
@@ -1,5 +0,0 @@
-const { json, listJSON, EVENT_PREFIX } = require("./_lib/store");
-exports.handler = async function () {
-  const events = await listJSON(EVENT_PREFIX, 100);
-  return json(200, { ok: true, count: events.length, events });
-};
