6bb0aae83f2c0ac267096ef74cb9e93a7730d45b
snbolin512-jpg <snbolin512@gmail.com>
2026-08-20 13:07:01 -0400

    Add files via upload

---
+++ ohmboy-health.js
@@ -0,0 +1,1 @@
+exports.handler=async function(){return{statusCode:200,headers:{"content-type":"application/json","access-control-allow-origin":"*"},body:JSON.stringify({ok:true,service:"ohmboy-health",version:"v0.21.6b-netlify-initialization-safe"},null,2)}};
